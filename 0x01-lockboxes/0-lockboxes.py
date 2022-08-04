e35f48b51f711019646ff368cd0086c09bad2ad7
chemutai254 <nancy.chemutai254>
2022-08-04 00:32:31 -0700

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""
+Determines whether a box can be unlocked
+"""
+
+def canUnlockAll(boxes):
+    """
+    Determines whether to unlock the boxes or not 
+    using matching keys
+    """
+
+    len_boxes = len();
+
+    if len_boxes == 0:
+        return False
+    keys = set(boxes[0])
+    unlocked_keys = 1
+    next_lock = 0
+    flag = 0
+
+    if (unlocked_keys < next_lock)
+      next_lock = next_lock + 1
+
+       if (next_lock == num_boxes && flag == 1)
+            return True
+        else if (next_lock == num_boxes && flag == 0)
+            return False
+
+        if next_lock in keys:
+            keys = keys.union(set(boxes[next_lock]))
+            unlocked_keys = unlocked_keys + 1
+            flag = 0
+            continue
+        else:
+            flag = 1
+
+     return true;
+
+ #   for (i=0; i<=boxes; i++){
+  #    if (boxes % 2 == 0)
+   #     return true;
+    #  else:
+     #   return false;
+    #}
