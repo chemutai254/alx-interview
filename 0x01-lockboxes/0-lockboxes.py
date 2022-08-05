47deb993516b9c13066a40eed1ac5a65b8e165df
chemutai254 <nancy.chemutai254>
2022-08-05 01:11:15 -0700

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,21 +7,24 @@ Determines whether a box can be unlocked
 
 def canUnlockAll(boxes):
     """Determines whether to unlock the boxes or not using matching keys """
+    num_boxes = len(boxes)
+    if num_boxes == 0:
+        return False
+    keys = set(boxes[0])
+    unlocked_keys = 1
+    next_lock = 0
+    flag = 0
 
- unlocked = [0]
-    for box_num, box in enumerate(boxes):
-        if not box:
+    while unlocked_keys < num_boxes:
+        next_lock = next_lock + 1
+        if next_lock == num_boxes:
+            return True if flag == 0 else False
+        if next_lock in keys:
+            keys = keys.union(set(boxes[next_lock]))
+            unlocked_keys = unlocked_keys + 1
+            flag = 0
             continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_num:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    return False
+        else:
+            flag = 1
 
-""" for (i=0; i<=boxes; i++){
-if (boxes % 2 == 0)
- return true;
- else:
-return false;
-}"""
+    return True
